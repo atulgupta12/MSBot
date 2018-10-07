ca9b181d2e595a300def8c0a9f287b7bf52eb3fe
Atul Gupta <P10417496@capita.co.in>
2018-10-07 11:19:57 +0530

    Initial Commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=searchbotsp-test-Web-Deploy.pubxml -p:Password=DjQflF0yZL6psqJ3vBilJHaAWwXviK5bBYthbcLLumLWwloLNLt1iv8Es4ji
+
